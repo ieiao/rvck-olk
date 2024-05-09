760cd69e8fd7ff95b1dccdb18b552a4ca2429e6d
Andy Chiu <andy.chiu@sifive.com>
2024-05-10 00:26:57 +0800

    riscv: vector: adjust minimum Vector requirement to ZVE32X
    mainline inclusion
    from mainline-6.11
    commit ac295b67422d1a6627866453543b4880ab144572 ("riscv: vector: adjust minimum Vector requirement to ZVE32X")
    category: feature
    Link: https://github.com/RVCK-Project/rvck/issues/354
    
    --------------------------------
    
    Make has_vector() to check for ZVE32X. Every in-kernel usage of V that
    requires a more complicate version of V must then call out explicitly.
    
    Also, change riscv_v_first_use_handler(), and boot code that calls
    riscv_v_setup_vsize() to accept ZVE32X.
    
    Most kernel/user interfaces requires minimum of ZVE32X. Thus, programs
    compiled and run with ZVE32X should be supported by the kernel on most
    aspects. This includes context-switch, signal, ptrace, prctl, and
    hwprobe.
    
    One exception is that ELF_HWCAP returns 'V' only if full V is supported
    on the platform. This means that the system without a full V must not
    rely on ELF_HWCAP to tell whether it is allowable to execute Vector
    without first invoking a prctl() check.
    
    Signed-off-by: Andy Chiu <andy.chiu@sifive.com>
    Acked-by: Joel Granados <j.granados@samsung.com>
    Link: https://lore.kernel.org/r/20240510-zve-detection-v5-7-0711bdd26c12@sifive.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    [zhenxing.zhu@linux.alibaba.com: partial backport, vector.h/cpufeature.c/vector.c already applied via baseline, only sys_hwprobe.c and uaccess.S hunks added]
    Signed-off-by: ZhenXing Zhu <zhenxing.zhu@linux.alibaba.com>
    [sync from rvck.]
    Signed-off-by: WeiHao Li <cn.liweihao@gmail.com>

--- arch/riscv/kernel/sys_hwprobe.c
+++ arch/riscv/kernel/sys_hwprobe.c
@@ -74,7 +74,7 @@ static void hwprobe_isa_ext0(struct riscv_hwprobe *pair,
 	if (riscv_isa_extension_available(NULL, c))
 		pair->value |= RISCV_HWPROBE_IMA_C;
 
-	if (has_vector())
+	if (has_vector() && riscv_isa_extension_available(NULL, v))
 		pair->value |= RISCV_HWPROBE_IMA_V;
 
 	/*
@@ -130,6 +130,10 @@ static void hwprobe_isa_ext0(struct riscv_hwprobe *pair,
 		EXT_KEY(ZKT);
 		EXT_KEY(ZTSO);
 
+		/*
+		 * All the following extensions must depend on the kernel
+		 * support of V.
+		 */
 		if (has_vector()) {
 			EXT_KEY(ZVBB);
 			EXT_KEY(ZVBC);
