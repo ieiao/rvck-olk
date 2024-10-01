8a4ed29c3e1e0563d13133ada921f45989aba3fa
Mark Brown <broonie@kernel.org>
2024-10-01 23:58:43 +0100

    prctl: arch-agnostic prctl for shadow stack
    mainline inclusion
    from mainline-6.13
    commit 91e102e79740 ("prctl: arch-agnostic prctl for shadow stack")
    category: feature
    Link: https://github.com/RVCK-Project/rvck/issues/255
    
    --------------------------------
    
    Three architectures (x86, aarch64, riscv) have announced support for
    shadow stacks with fairly similar functionality.  While x86 is using
    arch_prctl() to control the functionality neither arm64 nor riscv uses
    that interface so this patch adds arch-agnostic prctl() support to
    get and set status of shadow stacks and lock the current configuation to
    prevent further changes, with support for turning on and off individual
    subfeatures so applications can limit their exposure to features that
    they do not need.  The features are:
    
      - PR_SHADOW_STACK_ENABLE: Tracking and enforcement of shadow stacks,
        including allocation of a shadow stack if one is not already
        allocated.
      - PR_SHADOW_STACK_WRITE: Writes to specific addresses in the shadow
        stack.
      - PR_SHADOW_STACK_PUSH: Push additional values onto the shadow stack.
    
    These features are expected to be inherited by new threads and cleared
    on exec(), unknown features should be rejected for enable but accepted
    for locking (in order to allow for future proofing).
    
    This is based on a patch originally written by Deepak Gupta but modified
    fairly heavily, support for indirect landing pads is removed, additional
    modes added and the locking interface reworked.  The set status prctl()
    is also reworked to just set flags, if setting/reading the shadow stack
    pointer is required this could be a separate prctl.
    
    Reviewed-by: Thiago Jung Bauermann <thiago.bauermann@linaro.org>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Yury Khrustalev <yury.khrustalev@arm.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Reviewed-by: Deepak Gupta <debug@rivosinc.com>
    Link: https://lore.kernel.org/r/20241001-arm64-gcs-v13-4-222b78d87eee@kernel.org
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>
    Signed-off-by: ZhenXing Zhu <zhenxing.zhu@linux.alibaba.com>
    [sync from rvck.]
    Signed-off-by: WeiHao Li <cn.liweihao@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -4535,4 +4535,8 @@ static inline bool use_folio_cma(void)
 }
 #endif
 
+int arch_get_shadow_stack_status(struct task_struct *t, unsigned long __user *status);
+int arch_set_shadow_stack_status(struct task_struct *t, unsigned long status);
+int arch_lock_shadow_stack_status(struct task_struct *t, unsigned long status);
+
 #endif /* _LINUX_MM_H */
